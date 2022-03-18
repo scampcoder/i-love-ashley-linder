344d6671b5bf68a55be108c5249be0253f9582ea
Victoria Kee <victoriakee95@gmail.com>
2022-03-18 13:38:55 -0500

    set up background

---
+++ public/compliments.js
@@ -0,0 +1,14 @@
+import {nanoid} from 'nanoid'
+
+export default [
+    {
+        id: nanoid(),
+        type: "compliment",
+        text: "My beautiful goddess, I hope today is as special and magical as you are."
+    },
+    {
+        id: nanoid(),
+        type: "pep-talk",
+        text: "Hey bish. Whatever you're going through right now, you're gonna do great. You're amazing and I love you so much"
+    }
+]
