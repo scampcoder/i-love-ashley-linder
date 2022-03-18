ad10d744482d564a43a49f804ce496c0a6a77ae0
Victoria Kee <victoriakee95@gmail.com>
2022-03-18 16:20:37 -0500

    work on styling small window

--- src/compliments.js
+++ src/compliments.js
@@ -3,13 +3,49 @@ import {nanoid} from 'nanoid'
 const compliments = [
     {
         id: nanoid(),
-        type: "compliment",
         text: "My beautiful goddess, I hope today is as special and magical as you are."
     },
     {
         id: nanoid(),
-        type: "pep-talk",
         text: "Hey bish. Whatever you're going through right now, you're gonna do great. You're amazing and I love you so much"
+    },
+    {
+        id: nanoid(),
+        text: "Sweet baby Jesus, you got a beautiful face!"
+    },
+    {
+        id: nanoid(),
+        text: `Hey lady, guess what... I love you!`
+    },
+    {
+        id: nanoid(),
+        text: "If you ever have moments when you doubt yourself. Think about what an absolute badass you are. You have gone through so much and continue remain completely kickass."
+    },
+    {
+        id: nanoid(),
+        text: "Member that time we went to Colorado and climbed a damn mountain? That was cool. Majestic bugles."
+    },
+    {
+        id: nanoid(),
+        text: "I think you are the most amazing person I've ever planned to be a golden girl with. Rocking chairs and knitting, here we come."
+    },
+    {
+        id: nanoid(),
+        text: "Cheers to the smartest girl I know."
+    },
+    {
+        id: nanoid(),
+        text: "Hey you. You're doing amazing today. (And you look really hot too!)"
+    },
+    {
+        id: nanoid(),
+        text: "Here's a picture of a lumberjack to brighten your day.",
+        image: "https://i.pinimg.com/originals/a1/1c/ef/a11cefd353e434f5a43d9a4d3a0301aa.jpg",
+        alt: "Hot beefy lumberjack"
+    },
+    {
+        id: nanoid(),
+        text: "You're simply the breast! I mean best. You're the tits, lady."
     }
 ]
 
--- src/components/ComplimentComponent.js
+++ src/components/ComplimentComponent.js
@@ -3,11 +3,23 @@ import compliments from '../compliments'
 
 export default function Compliment() {
     const randomIndex = Math.floor(Math.random() * compliments.length)
-    const complimentText = compliments[randomIndex].text;
-
-    return (
-        <div className='compliment-container'>
-            <p className='compliment'>{complimentText}</p>
-        </div>
-    )
-}
+    
+        if(compliments[randomIndex].hasOwnProperty('image')) {
+            console.log("image found")
+            return (
+                <div className='compliment-container'>
+                    <p className='compliment'>{compliments[randomIndex].text} 
+                        <br />
+                        <img src={compliments[randomIndex].image} alt={compliments[randomIndex].alt} className="pics"/>
+                    </p>
+               </div>
+            )
+        } else {
+            console.log("image not found")
+            return (
+                <div className='compliment-container'>
+                    <p className='compliment'>{compliments[randomIndex].text}</p>
+               </div>
+            )
+        }
+    }
